2e01df021f99786ea96ededa36d407ac45a038e7
Enakhe <izuagbesam@gmail.com>
2025-06-20 02:08:55 +0100

    Fixed user detals after authentication

--- src/api/userSlice.ts
+++ src/api/userSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-const baseUrl = "http://localhost:5000/api"
+const baseUrl = "https://nolly-watch-backend.vercel.app/api"
 
 export interface User {
     _id: string
